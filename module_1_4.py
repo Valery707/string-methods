7c2a73910caeee22e7560a41ae1791cf137ceeb0
Valery707 <skuba707@gmail.com>
2024-11-24 14:58:53 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('введите произвольный текст: ')
+print('в тексте "',my_string,'"',len(my_string),"символов")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
